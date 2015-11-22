b0256cc9e503901863c0f2b55bed13244f2ef7f0
LinKunxin <linkunxin@linkunxindemacbook-pro.local>
2015-11-22 08:55:22 +0800

    setup project

---
+++ CoolGameIOSDK/CoolGameIOSDK/CoolGameIOSDK.h
@@ -0,0 +1,13 @@
+//
+//  CoolGameIOSDK.h
+//  CoolGameIOSDK
+//
+//  Created by LinKunxin on 15/11/22.
+//  Copyright © 2015年 kuyou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoolGameIOSDK : NSObject
+
+@end
